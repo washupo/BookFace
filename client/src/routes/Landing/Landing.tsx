2923abec9da06901e1962dd272a42f92c84838da
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2024-01-15 15:35:56 +0100

    Update landing.tsx (gap-30 + max-h-screen)

--- client/src/routes/Landing/Landing.tsx
+++ client/src/routes/Landing/Landing.tsx
@@ -1,15 +1,15 @@
 import { Layout } from "../../components/layout/Layout";
 import logo from "../../assets/images/logo.svg";
-import { LoginForm } from "../../features/LoginForm";
+import { SignUpForm } from "../../features/SignUpForm";
 import { Button } from "../../common/button";
 
 export default function Landing() {
   return (
     <>
       <Layout background="brown">
-        <div className="h-screen py-30 px-20 flex flex-col justify-between items-center">
+        <div className="min-h-screen py-30 px-20 flex flex-col justify-between items-center gap-y-30">
           <img src={logo} alt="Logo Funny Fur" />
-          <LoginForm />
+          <SignUpForm />
           <Button
             type="submit"
             background="white"
