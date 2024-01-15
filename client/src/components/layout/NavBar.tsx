518e563bac7721c8ba72c400943d3fd8ad75ac96
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2024-01-15 23:24:32 +0100

    Update NavBar

--- client/src/components/layout/NavBar.tsx
+++ client/src/components/layout/NavBar.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
-
 import { Icon } from '../Icon';
 import { Link } from 'react-router-dom';
-import { Modal } from '../../common/modal';
+import { IconButton } from '../../common/IconButton';
 
 import profilPicture from '../../assets/images/profilPictureTest.png';
+
+import { SearchModal } from '../../pages/PopUps/SearchModal';
+import { PostModal } from '../../pages/PopUps/NewPostModal';
+import { NotifModal } from '../../pages/PopUps/NotificationModal';
 
 type NavBarProps = {
     className?: string;
@@ -12,28 +15,29 @@ type NavBarProps = {
 };
 
 export const NavBar = ({ className }: NavBarProps) => {
-    const [showModal, setShowModal] = useState(false)
-    const handleClose = () => { setShowModal(false) }
+    const [popUpSearch, setPopUpSearch] = useState(false);
+    const [popUpPost, setPopUpPost] = useState(false);
+    const [popUpNotif, setPopUpNotif] = useState(false);
+
+    const searchPopUp = () => setPopUpSearch(true);
+    const postPopUp = () => setPopUpPost(true);
+    const notifPopUp = () => setPopUpNotif(true);
 
     return (
         <>
-            <div className={`flex flex-col w-full items-start gap-30 p-30 bg-brownPrimary border-t-2 border-whitePrimary z-50 fixed h-auto bottom-0 ${className}`}>
+            <div className={`flex flex-col w-full items-start gap-30 p-30 bg-brownPrimary border-t-2 border-whitePrimary fixed h-auto bottom-0 ${className}`}>
                 <div className="flex items-center justify-between w-full">
+
                     <Link to="/homepage">
                         <Icon name='home' size='small' fill='white' />
                     </Link>
 
-                    <button onClick={() => setShowModal(true)}>
-                        <Icon name='search' size='small' fill='white' />
-                    </button>
+                    <IconButton onClick={searchPopUp} name='search' size='small' fill='white' />
 
-                    <Link to="/">
-                        <Icon name='post' size='small' fill='white' />
-                    </Link>
+                    <IconButton onClick={postPopUp} name='post' size='small' fill='white' />
 
-                    <Link to="/">
-                        <Icon name='like' size='small' fill='white' />
-                    </Link>
+                    <IconButton onClick={notifPopUp} name='like' size='small' fill='white' />
+
                     <Link to="/profile">
                         <img className=" w-8 h-8 rounded-full" alt="Profile picture" src={profilPicture} />
                     </Link>
@@ -41,7 +45,10 @@ export const NavBar = ({ className }: NavBarProps) => {
                 </div>
             </div>
 
-            {showModal && <Modal name={name} role={role} skills={skills} profilePic={profilePic} handleClose={handleClose}/>}
+            {popUpSearch && (<SearchModal isOpen={popUpSearch} setIsOpen={setPopUpSearch} />)}
+            {popUpPost && (<PostModal isOpen={popUpPost} setIsOpen={setPopUpPost} />)}
+            {popUpNotif && (<NotifModal isOpen={popUpNotif} setIsOpen={setPopUpNotif} />)}
+
         </>
     );
 }
