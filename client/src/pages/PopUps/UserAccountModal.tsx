61b1ef12339e3e0d0a83c3d45060da52dd2bd4cf
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2024-01-16 23:44:44 +0100

    add UserAccountModal.tsx

--- client/src/pages/PopUps/UserAccountModal.tsx
+++ client/src/pages/PopUps/UserAccountModal.tsx
@@ -1,5 +1,8 @@
-
 import { Modal } from "../../common/Modal";
+import { UserAccountForm } from "../UserProfile/UserAccountForm";
+
+import profilPicture from '../../assets/images/profilPictureTest.png';
+
 
 interface UserModalProps {
     isOpen: boolean;
@@ -9,18 +12,24 @@ interface UserModalProps {
 }
 
 export const UserModal = ({ isOpen, setIsOpen, className }: UserModalProps) => {
-    
+
     return (
         <Modal
             isOpen={isOpen}
             setIsOpen={setIsOpen} 
             background='white'
-            className={`${className}`}
-            name="Modal Title"
+            className={`${className} left-0 overflow-y-scroll`}
+            name="Compte utilisateur"
             textColor="brown"
             fill="brown"
         >
-                    <p>Ceci est un exemple de contenu de modal.</p>
+            <div className="flex flex-col items-center justify-center gap-30 pt-30">
+            <img className="rounded-full" alt="Profile picture" src={profilPicture} />
+            {/* <img className="rounded-full" alt="Profile picture" src={profilPicture} /> */}
+
+            <UserAccountForm />
+            </div>
+            
 
 
         </Modal>
