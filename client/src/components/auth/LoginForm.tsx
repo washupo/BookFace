924b504b31755359980263520adb1c6cda2556fb
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2024-01-17 11:22:23 +0100

    update axios + Fix Input/LoginForm

--- client/src/components/auth/LoginForm.tsx
+++ client/src/components/auth/LoginForm.tsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
+import axios from "axios";
+
 import { Input } from "../form/Input";
 import { Form } from "../form/Form";
 import { Button } from "../../common/button";
 
 export const LoginForm = (): JSX.Element => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("/api/auth/login",
+        credentials
+      );
+
+      // const response = await axios.post("/auth/login",
+      //   credentials
+      // );
+
+      const { token, refreshToken } = response.data;
+
+      localStorage.setItem("token", token);
+      localStorage.setItem("refreshToken", refreshToken);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Form onSubmit={() => { }}>
+    <Form onSubmit={handleSubmit}>
       <div className="w-full flex flex-col gap-25">
         <Input
           as="input"
@@ -12,10 +48,10 @@ export const LoginForm = (): JSX.Element => {
           placeholder="Nom d'utilisateur"
           type="text"
           name="username"
-          value=""
+          value={credentials.username}
           color="white"
           border="bottom"
-          onChange={() => { }}
+          onChange={handleChange}
         />
 
         <Input
@@ -24,10 +60,10 @@ export const LoginForm = (): JSX.Element => {
           placeholder="*********"
           type="text"
           name="username"
-          value=""
+          value={credentials.password}
           color="white"
           border="bottom"
-          onChange={() => { }}
+          onChange={handleChange}
         />
       </div>
       <Button type="submit" background="white" name="Connexion" />
