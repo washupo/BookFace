fda08c306aef4892cbcdb5d3e3a1534ab15e7e88
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2024-01-18 23:28:47 +0100

    update root.tsx

--- client/src/root.tsx
+++ client/src/root.tsx
@@ -1,55 +1,11 @@
-import { useState, useEffect } from 'react';
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
-interface OutletProps {
-  openModal?: () => void;
-  closeModal?: () => void;
-}
+export const CustomOutlet = () => {
 
-export const CustomOutlet: React.FC<OutletProps> = ({ openModal, closeModal  }) => {
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const location = useLocation();
-
-  // Ajuster le comportement de défilement en fonction de l'état de la modal
-  useEffect(() => {
-    if (isModalOpen) {
-      // Si la modal est ouverte, désactivez le défilement de la page
-      document.body.style.overflow = 'hidden';
-    } else {
-      // Sinon, réactivez le défilement
-      document.body.style.overflow = 'auto';
-    }
-
-    // Nettoyer l'effet lorsque le composant est démonté ou lorsque l'emplacement change
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-
-  }, [isModalOpen, location.pathname]);
-
-  //Fonction pour ouvrir la modal
-  function handleOpenModal() {
-    setIsModalOpen(true);
-    if (openModal) {
-      openModal();
-    }
-  }
-
-  //Fonction pour fermer la modal
-  function handleCloseModal() {
-    setIsModalOpen(false);
-    if (closeModal) {
-      closeModal();
-    }
-  }
 
   return (
-    <>
-      <Outlet 
-      openModal={handleOpenModal} 
-      closeModal={handleCloseModal} 
-      />
+    <> 
+      <Outlet />
     </>
   )
 }
