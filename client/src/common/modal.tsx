ccdf76cee75dfc320b2a0ef7d6ee186b8fb3111f
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2024-01-19 01:31:34 +0100

    update modal.tsx

--- client/src/common/modal.tsx
+++ client/src/common/modal.tsx
@@ -29,9 +29,14 @@ export const Modal = ({
   const textColorClass =
     textColor === "brown" ? "text-brownPrimary" : "text-whitePrimary";
 
+    // Utilisez la classe modal-open pour désactiver/activer le défilement
+  const modalClass = `modal-container ${backgroundClass} ${textColorClass} ${className} fixed top-0 py-30 px-20 z-50 min-h-screen w-screen ${
+    document.body.classList.contains("modal-open") ? "modal-open" : ""
+  }`;
+
   return (
     <div
-      className={`${backgroundClass} ${textColorClass} ${className} fixed top-0 py-30 px-20 z-50 min-h-screen w-screen`}
+      className={modalClass}
     >
       <hgroup className="flex justify-between pb-30">
         <Typography component="h2" fontFamily="FKGroteskBold" fontSize="20">
@@ -44,7 +49,7 @@ export const Modal = ({
           fill={fill}
         />
       </hgroup>
-      <div className="w-full flex flex-col gap-30 min-h-screen overflow-y-auto">
+      <div className="w-full flex flex-col gap-30 min-h-screen">
         {children}
       </div>
     </div>
