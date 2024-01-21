a0456499a26d339dfcc955bc2e0a7478ca24d4da
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2024-01-21 18:52:51 +0100

    update personal page

--- client/src/pages/PersonalPage/PersonalPage.tsx
+++ client/src/pages/PersonalPage/PersonalPage.tsx
@@ -1,29 +1,110 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { IconButton } from "../../common/IconButton";
 import { Typography } from "../../common/Typography";
 import { Layout } from "../../components/layout/Layout";
 import { NavBar } from "../../components/layout/NavBar";
+import { disableBodyScroll, enableBodyScroll } from "../../bodyscroll";
 
-import { UserModal } from "../PopUps/UserAccountModal";
-import { disableBodyScroll, enableBodyScroll } from "../../BodyScroll";
+import axios from "axios";
+import { api } from "../../API/api";
 
-import profilPicture from '../../assets/images/profilPictureTest.png';
+interface ProfilDataProps {
+  _id: string;
+  username: string;
+  email: string;
+  birthdate: string;
+  species: string;
+  bio: string;
+  avatar: {
+    url: string;
+  }
+}
 
-
+interface PostProps {
+  id: string;
+  title: string;
+  url: string;
+  user: number;
+  description: string;
+}
 
 export default function PersonalPage() {
   const [popUpUser, setPopUpUser] = useState(false);
+  const [profileDatas, setProfileDatas] = useState<ProfilDataProps[]>([]);
+  const [posts, setPosts] = useState<PostProps[]>([]);
 
   const openPopUpUser = () => {
     setPopUpUser(true);
-    disableBodyScroll({ savePosition: true });  
+    disableBodyScroll({ savePosition: true });
   };
 
   const closePopUpUser = () => {
     setPopUpUser(false);
-    enableBodyScroll();  
+    enableBodyScroll();
   };
+
+  useEffect(() => {
+    // Récupérer les données du profil
+    const getProfileDatas = async () => {
+
+      try {
+        // Envoyer requête GET
+        const response = await api.get(
+          "http://localhost:8000/profile",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            }
+          }
+        );
+
+        // Handle the case where the response is a string
+        if (typeof response.data === 'string') {
+          console.log('Unexpected response format:', response.data);
+          return null;
+        }
+
+        // Gérer réponse - Mise à jour de l'état local avec les données du profil
+        setProfileDatas(Array.isArray(response.data) ? response.data : [response.data]);
+        console.log("Profile :", response.data);
+      } catch (error) {
+        // Gérer erreur
+        if (axios.isAxiosError(error)) {
+          console.error('Échec de l\'authentification :', error.response?.data?.message);
+        } else {
+          console.error('An unexpected error occurred:', error);
+        }
+      }
+    }
+
+    const getPosts = async () => {
+      try {
+        const response = await axios.get('https://api.slingacademy.com/v1/sample-data/photos?limit=50')
+        const posts = response.data
+        console.log(response.data)
+
+        // Handle the case where the response is a string
+        if (posts && posts.success) {
+          setPosts(posts.photos);
+        } else {
+          console.error('Unexpected response format:', response.data);
+        }
+      } catch (error) {
+        // Gérer erreur
+        console.error('An unexpected error occurred:', error);
+      }
+    }
+    // Call the function to fetch data when the component mounts
+    getProfileDatas();
+    getPosts();
+  }, []);
+
+  const onClick = () => {
+    setPostPopUp(true);
+    disableBodyScroll({ savePosition: true });
+    console.log("click")
+  }
 
   return (
     <>
@@ -31,77 +112,71 @@ export default function PersonalPage() {
         background="white"
         className="min-h-screen py-30 px-20 flex flex-col gap-30 pb-36"
       >
+        {profileDatas.map((profileData) => (
+          <div 
+          className="flex flex-col gap-30"
+          key={profileData._id}>
+            <hgroup className="flex justify-between">
+              <Typography component='h2' fontFamily="FKGroteskBold" textColor="brown" fontSize="20">{profileData.username}</Typography>
 
-        <hgroup className="flex justify-between">
-          <Typography component='h2' fontFamily="FKGroteskBold" textColor="brown" fontSize="20">username</Typography>
-          {/* <Typography component='h2' fontFamily="FKGroteskBold" fontSize="20">{username}</Typography> */}
-          <IconButton onClick={openPopUpUser} name='settings' size='small' fill='brown' />
-        </hgroup>
+              {/* <Typography component='h2' fontFamily="FKGroteskBold" fontSize="20">{username}</Typography> */}
+              <IconButton onClick={openPopUpUser} name='settings' size='small' fill='brown' />
+            </hgroup>
 
-        <div className="flex flex-col gap-15">
-          <div className="flex gap-10 items-center">
+            <div className="flex flex-col gap-15">
+              <div className="flex gap-10 items-center">
 
-            <img className=" w-20 h-20 rounded-full" alt="Profile picture" src={profilPicture} />
+                <img className=" w-20 h-20 rounded-full" alt="Profile picture" src="https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nfGVufDB8fDB8fHww" />
+                {/* <img className=" w-20 h-20 rounded-full" alt="Profile picture" src={profileData.avatar.url} /> */}
 
-            <div className="flex justify-between w-full">
-              <div>
-                <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">999M</Typography>
-                {/* <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">{postsNumber}</Typography> */}
-                <Typography component='p' fontFamily="FKGrotesk" fontSize="15" textColor="brown">Publications</Typography>
+                <div className="flex justify-between w-full">
+                  <div>
+                    <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">999M</Typography>
+                    {/* <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">{postsNumber}</Typography> */}
+                    <Typography component='p' fontFamily="FKGrotesk" fontSize="15" textColor="brown">Publications</Typography>
+                  </div>
+                  <div>
+                    <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">999M</Typography>
+                    {/* <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">{followersNumber}</Typography> */}
+                    <Typography component='p' fontFamily="FKGrotesk" fontSize="15" textColor="brown">Followers</Typography>
+                  </div>
+                  <div>
+                    <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">999M</Typography>
+                    {/* <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">{followNumber}</Typography> */}
+                    <Typography component='p' fontFamily="FKGrotesk" fontSize="15" textColor="brown">Suivi(e)s</Typography>
+                  </div>
+                </div>
               </div>
-              <div>
-                <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">999M</Typography>
-                {/* <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">{followersNumber}</Typography> */}
-                <Typography component='p' fontFamily="FKGrotesk" fontSize="15" textColor="brown">Followers</Typography>
-              </div>
-              <div>
-                <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">999M</Typography>
-                {/* <Typography component='p' fontFamily="FKGroteskBold" fontSize="20" textColor="brown" className="text-center">{followNumber}</Typography> */}
-                <Typography component='p' fontFamily="FKGrotesk" fontSize="15" textColor="brown">Suivi(e)s</Typography>
-              </div>
+
+              <section>
+                <Typography component='p' fontFamily="FKGroteskBold" fontSize="15" textColor="brown">{profileData.username}</Typography>
+                <Typography component='p' fontFamily="FKGrotesk" fontSize="15" textColor="brown">
+                  {profileData.bio}
+                </Typography>
+              </section>
+
+              <button className=" bg-brownPrimary text-whitePrimary font-fkGroteskBold py-1 px-9 rounded-5 ml-0 m-auto">
+                suivre
+              </button>
+              {/* onClick={ } */}
+            </div>
+
+            <div className="grid grid-cols-3 gap-1">
+              {posts
+              .filter((post) => post.user === 30)
+              .map((post) => (
+                <div key={post.id}  onClick={onClick}>
+                  <img className="w-full h-full object-cover" alt="Profile picture" src={post.url} />
+                </div>  
+              ))}
             </div>
 
           </div>
-
-          <section>
-            <Typography component='p' fontFamily="FKGroteskBold" fontSize="15" textColor="brown">username</Typography>
-            {/* <Typography component='p' fontFamily="FKGroteskBold" fontSize="15" textColor="brown">username</Typography> */}
-            <Typography component='p' fontFamily="FKGrotesk" fontSize="15" textColor="brown">
-              Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Typography>
-            {/* <Typography component='h2' fontFamily="FKGroteskBold" fontSize="15" textColor="brown" >{description}</Typography> */}
-          </section>
-
-          <button className=" bg-brownPrimary text-whitePrimary font-fkGroteskBold py-1 px-9 rounded-5 ml-0 m-auto">
-            suivre
-          </button>
-          {/* onClick={ } */}
-        </div>
-
-          <div className="bg-red-400 h-[300vh]">
-          <span>...</span>
-        </div>
-
-        {/*<div className='flex gap-3'>
-        {menus.map(menu => (
-          <div
-            key={menu._id}
-            className='text-center space-y-2 shadow-xl rounded-2xl w-1/2 sm:w-1/3 h-3/4'
-          >
-            <img
-              src={menu.image}
-              alt={menu.name}
-              className='object-cover rounded-t-2xl h-3/5 w-full'
-            />
-            <h2 className='font-luckiest text-orange'>{menu.name}</h2>
-            <p className='text-sm italic'>{menu.description}</p>
-            <p>{menu.price}</p>
-          </div>
         ))}
-        </div>*/}
+
       </Layout>
       <NavBar />
-      {popUpUser && <UserModal handleCloseModal={closePopUpUser}  />}
+      {popUpUser && <Post handleCloseModal={closePopUpUser} />}
     </>
   )
 } 
