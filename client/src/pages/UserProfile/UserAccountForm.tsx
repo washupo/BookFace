d483d23f1b1b792cca13c6cec21b3251f76c1114
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2024-01-21 16:07:49 +0100

    Refactor UserAccountForm component

--- client/src/pages/UserProfile/UserAccountForm.tsx
+++ client/src/pages/UserProfile/UserAccountForm.tsx
@@ -4,15 +4,11 @@ import { Form } from "../../components/form/Form";
 import { Button } from "../../common/button";
 import { api } from "../../API/api";
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
-
-
-interface UserAccountFormProps {
-  onSubmit?: () => void;
-}
 
 interface ProfilDataProps {
-  _id: string;
+  userId: string;
   avatar: {
     url: string;
   }
@@ -25,26 +21,27 @@ interface ProfilDataProps {
   password: string;
 }
 
-export const UserAccountForm = ({ onSubmit }: UserAccountFormProps): JSX.Element => {
-
-  // const [user, setUser] = useState(null);
-
-  // useEffect(() => {
-  //   const fetchUser = async () => {
-  //     try {
-  //       const response = await api.get("/profile");
-  //       setUser(response.data);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   fetchUser();
-  // }, []);
-
-  // if (!user) {
-  //   return <div>Loading...</div>; }
+export const UserAccountForm = (): JSX.Element => {
 
   const [profileDatas, setProfileDatas] = useState<ProfilDataProps[]>([]);
+
+  const [credentials, setCredentials] = useState({
+    bio: "",
+    username: "",
+    email: "",
+    birthdate: "",
+    species: "",
+    gender: "",
+    password: "",
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
 
   useEffect(() => {
     // Récupérer les données du profil
@@ -69,6 +66,7 @@ export const UserAccountForm = ({ onSubmit }: UserAccountFormProps): JSX.Element
 
         // Mettre à jour les données du profil
         setProfileDatas(Array.isArray(response.data) ? response.data : [response.data]);
+        
         console.log("Profile :", response.data);
       } catch (error) {
         // Gérer erreur
@@ -79,111 +77,155 @@ export const UserAccountForm = ({ onSubmit }: UserAccountFormProps): JSX.Element
         }
       }
     };
-
     getProfileDatas();
   }, []);
 
-  return (
-    <Form onSubmit={onSubmit  || (() => {})}>
-      {profileDatas.map((profileData) => (
-      <div className="w-full flex flex-col gap-25">       
-        <Input
-          as="textarea"
-          label="Bio"
-          placeholder="Ajoutez une petite description"
-          type="text"
-          name="username"
-          value=""
-          color="brown"
-          border="all"
-          gap={true}
-          onChange={() => { }}
-        />
-        <Input
-          as="input"
-          label="Nom d'utilisateur"
-          placeholder={profileData.username}
-          type="text"
-          name="username"
-          value=""
-          color="brown"
-          border="bottom"
-          onChange={() => { }}
-        />
-        <Input
-          as="input"
-          label="Espèce"
-          placeholder={profileData.species}
-          type="text"
-          name="species"
-          value=""
-          color="brown"
-          border="bottom"
-          onChange={() => { }}
-        />
-        <div className="flex gap-x-25">
-          <Input
-            as="input"
-            label="Genre"
-            placeholder={profileData.gender}
-            type="text"
-            name="species"
-            value=""
-            color="brown"
-            border="bottom"
-            className="w-1/4"
-            onChange={() => { }}
-          />
+  const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-          <Input
-            as="input"
-            label="Date de naissance"
-            placeholder={profileData.birthdate}
-            type="text"
-            name="species"
-            value=""
-            color="brown"
-            border="bottom"
-            className="w-3/4 "
-            onChange={() => { }}
-          />
-        </div>
-        <Input
-          as="input"
-          label="Adresse email"
-          placeholder={profileData.email}
-          type="text"
-          name="species"
-          value=""
-          color="brown"
-          border="bottom"
-          onChange={() => { }}
-        />
-        <Input
-          as="input"
-          label="Mot de passe"
-          placeholder="*********"
-          type="text"
-          name="username"
-          value=""
-          color="brown"
-          border="bottom"
-          onChange={() => { }}
-        />
-        <Input
-          as="input"
-          label="Ressaisir mot de passe"
-          placeholder="*********"
-          type="text"
-          name="username"
-          value=""
-          color="brown"
-          border="bottom"
-          onChange={() => { }}
-        />
-        
-      </div>
-      ))}
+    try {
+
+      const formData = new FormData();
+      formData.append("bio", credentials.bio);
+      formData.append("username", credentials.username);
+      formData.append("email", credentials.email);
+      formData.append("birthdate", credentials.birthdate);
+      formData.append("species", credentials.species);
+      formData.append("password", credentials.password);
+
+      const decodedToken: any = jwtDecode(localStorage.getItem("token") as string);
+      const userId = decodedToken._id;
+      console.log("userId :", userId);
+
+      const response = await axios.put(`http://localhost:8000/profile/${userId}`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          }
+        }
+      );
+
+      // Vérifier que la requête est réussie, vous pouvez traiter la réponse ici
+      console.log("Profil mis à jour :", response.data);
+
+      // Alerte succès
+      alert("Profil mis à jour avec succès !");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error(error.response?.data?.message);
+      } else {
+        console.error('An unexpected error occurred:', error);
+      }
+    } finally {
+      // Clear the user's ID from the headers to avoid affecting other requests
+      delete api.defaults.headers.common["X-User-Id"];
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+{profileDatas.map((profileData) => (
+  <div 
+    className="w-full flex flex-col gap-25"
+    key={profileData.userId}
+  >
+    <Input
+      as="textarea"
+      label="Bio"
+      value={profileData.bio}
+      placeholder={profileData.bio}
+      type="text"
+      name="bio"
+      color="brown"
+      border="all"
+      gap={true}
+      onChange={handleChange}
+    />
+    <Input
+      as="input"
+      label="Nom d'utilisateur"
+      value={profileData.username} 
+      placeholder={profileData.username}
+      type="text"
+      name="username"
+      color="brown"
+      border="bottom"
+      onChange={handleChange}
+    />
+    <Input
+      as="input"
+      label="Espèce"
+      value={profileData.species}
+      placeholder={profileData.species} 
+      type="text"
+      name="species"
+      color="brown"
+      border="bottom"
+      onChange={handleChange}
+    />
+    <div className="flex gap-x-25">
+      <Input
+        as="input"
+        label="Genre"
+        value={profileData.gender} 
+        placeholder={profileData.gender}
+        type="text"
+        name="species"
+        color="brown"
+        border="bottom"
+        className="w-1/4"
+        onChange={handleChange}
+      />
+      <Input
+        as="input"
+        label="Date de naissance"
+        value={profileData.birthdate}
+        placeholder={profileData.birthdate} 
+        type="text"
+        name="species"
+        color="brown"
+        border="bottom"
+        className="w-3/4 "
+        onChange={handleChange}
+      />
+    </div>
+    <Input
+      as="input"
+      label="Adresse email"
+      value={profileData.email}
+      placeholder={profileData.email} 
+      type="text"
+      name="species"
+      color="brown"
+      border="bottom"
+      onChange={handleChange}
+    />
+    <Input
+      as="input"
+      label="Mot de passe"
+      value={profileData.password}
+      placeholder="*********" 
+      type="password"
+      name="password"
+      color="brown"
+      border="bottom"
+      onChange={handleChange}
+    />
+    <Input
+      as="input"
+      label="Ressaisir mot de passe"
+      value={profileData.password}
+      placeholder="*********"
+      type="password"
+      name="password"
+      color="brown"
+      border="bottom"
+      onChange={handleChange}
+    />
+  </div>
+))}
+
       <Button type="submit" background="brown" name="Connexion" />
     </Form>
   );
